917e37a7158bb87949319f5dc85dd342e15d899b
NinoLoncar <nino.loncar16@gmail.com>
2025-01-01 17:58:48 +0100

    Add registration

---
+++ PlantApp/User Controls/RegistrationUC.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Npgsql;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PlantApp
+{
+	/// <summary>
+	/// Interaction logic for RegistrationUC.xaml
+	/// </summary>
+	public partial class RegistrationUC : UserControl
+	{
+		DatabaseHelper db;
+		public RegistrationUC()
+		{
+			InitializeComponent();
+			db = new DatabaseHelper();
+		}
+
+		private void btnRegister_Click(object sender, RoutedEventArgs e)
+		{
+			var username = txtUsername.Text;
+			var password = txtPassword.Password;
+			var email = txtEmail.Text;
+
+			if (username == string.Empty || password == string.Empty || email == string.Empty)
+			{
+				lblLogin.Content = "Fill the form";
+				lblLogin.Visibility = Visibility.Visible;
+				lblLogin.Foreground = Brushes.Red;
+				return;
+			}
+			try
+			{
+				var sql = "INSERT INTO users VALUES (@Email, @Username, @Password);";
+				var result = db.ExecuteNonQuery(sql, new { Email = email, Username = username, Password = password });
+
+				if (result == 1)
+				{
+					lblLogin.Content = "Successful registration";
+					lblLogin.Visibility = Visibility.Visible;
+					lblLogin.Foreground = Brushes.Green;
+				}
+				else
+				{
+					lblLogin.Content = "Something went wrong";
+					lblLogin.Visibility = Visibility.Visible;
+					lblLogin.Foreground = Brushes.Red;
+				}
+			}
+			catch (PostgresException ex)
+			{
+				HandlePostgresError(ex);
+			}
+			catch (Exception ex)
+			{
+				lblLogin.Content = "Something went wrong";
+				lblLogin.Visibility = Visibility.Visible;
+				lblLogin.Foreground = Brushes.Red;
+			}
+
+		}
+		private void HandlePostgresError(PostgresException ex)
+		{
+			lblLogin.Visibility = Visibility.Visible;
+			lblLogin.Foreground = Brushes.Red;
+			switch (ex.SqlState)
+			{
+				case "23505":
+					lblLogin.Content = "Email already registered";
+					break;
+				case "P0001":
+					lblLogin.Content = "Wrong email format";
+					break;
+				default:
+					lblLogin.Content = "Something went wrong";
+					break;
+			}
+		}
+	}
+}
