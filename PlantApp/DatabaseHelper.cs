6e0770aaebc0845d9691857d3cf06b2a79d1dbae
NinoLoncar <nino.loncar16@gmail.com>
2025-01-01 17:57:55 +0100

    Create DatabaseHelper.cs

---
+++ PlantApp/DatabaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Npgsql;
+
+namespace PlantApp
+{
+	public class DatabaseHelper
+	{
+		private readonly string _connString = "Host=localhost;Username=postgres;Password=postgres;Database=plants";
+
+		public DatabaseHelper()
+		{
+		}
+
+		public IEnumerable<T> ExecuteQuery<T>(string sql, object? parameters = null)
+		{
+			using (var connection = new NpgsqlConnection(_connString))
+			{
+				connection.Open();
+				return connection.Query<T>(sql, parameters);
+			}
+		}
+		public int ExecuteNonQuery(string sql, object? parameters = null)
+		{
+			using (var connection = new NpgsqlConnection(_connString))
+			{
+				connection.Open();
+				return connection.Execute(sql, parameters);
+			}
+		}
+	}
+}
