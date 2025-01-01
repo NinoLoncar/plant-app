cea520ae02fb13aeff9612816f789964ac97b595
NinoLoncar <nino.loncar16@gmail.com>
2025-01-01 22:41:27 +0100

    Add notifications ordering

--- PlantApp/User Controls/Notifications.xaml.cs
+++ PlantApp/User Controls/Notifications.xaml.cs
@@ -15,7 +15,7 @@ namespace PlantApp.User_Controls
 
 		private void LoadData()
 		{
-			var sql = "SELECT message, created_at createdAt FROM notifications WHERE receiver = @Email;";
+			var sql = "SELECT message, created_at createdAt FROM notifications WHERE receiver = @Email ORDER BY created_at DESC;";
 			var notifications = db.ExecuteQuery<Notification>(sql, new { Email = Authenticator.LoggedInUser.Email }).ToList();
 			dtgNotifications.ItemsSource = notifications;
 		}
